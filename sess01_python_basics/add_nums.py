df8f769a9b584aff01417364ceefb560d1e7dbc6
Salim Talal <s.talal@edulink.ac.ke>
2025-12-11 14:58:10 +0300

    Add the greet_user.py and add_nums.py files

---
+++ sess01_python_basics/add_nums.py
@@ -0,0 +1,14 @@
+# Python script to get two nubers from the user add them and display their sum
+
+
+# Prompt/ask the user for the first number
+num1 = int(input('Enter first number: \n'))
+
+# Prompt/ask ther user for the second number
+num2 = int(input('Enter second number: \n'))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Display the result
+print(f'The sum of {num1} and {num2} is {sum}')
---
+++ sess01_python_basics/greet_user.py
@@ -0,0 +1,6 @@
+# Python script to accept the user's name and greet them
+name = input("Enter your name: \n")
+print("Hello, " + name)
+
+# Display the type of the nema variable
+print(f'The type of name variable is {type(name)}')
